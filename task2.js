c0aedd94bc317d6f5f386edda028fcf433d2887b
Макс Доши <johnsmalest@gmail.com>
2022-03-13 22:17:33 +0300

    f

--- task2.js
+++ task2.js
@@ -1,13 +1,14 @@
 function deepObjectCopy(obj) {
   if (typeof obj === 'object') {
     let cloneObj = Array.isArray(obj) ? [] : {};
+
     for (let key in obj) {
       cloneObj[key] = deepObjectCopy(obj[key]);
     }
+
     return cloneObj;
-  } else {
-    return obj;
   }
+  return obj; 
 }
 
 
